8ff419317b5908e151f7bcdb8315c1a3e1d78cbe
zeigerb <65622917+zeigerb@users.noreply.github.com>
2025-10-28 18:40:11 -0400

    added general comments

--- prelim_analysis.R
+++ prelim_analysis.R
@@ -1,19 +1,40 @@
 
 library(readxl)
-fly_df <- read_excel("Fly_Collection_Data.xlsx", sheet = "count_data")
+taban_df <- read_excel("taban_Collection_Data.xlsx", sheet = "count_data")
 
-View(fly_df)
+View(taban_df)
 
-summary(fly_df)
+#factorize categorical data
+taban_df$site <- as.factor(taban_df$site)
+taban_df$type <- as.factor(taban_df$type)
+
+summary(taban_df)
+
 
 library(tidyverse)
 
-fly_df %>% group_by(site) %>%
-  summarise(mean(count))
+#means and sd when grouped by study site
+taban_df %>% group_by(site) %>%
+  summarise(meanC = mean(count),
+            sdC = sd(count))
 
-fly_df %>% group_by(type) %>%
-  summarise(mean(count))
+#means and sd when grouped by trap placement type
+taban_df %>% group_by(type) %>%
+  summarise(meanC = mean(count),
+            sdC = sd(count))
 
-fly_df %>% ungroup()
+taban_df %>% ungroup()
+
+
+#histogram of all observations
+hist(taban_df$count)
+
+#plot of count vs GDD
+plot(count ~ GDD, data = taban_df, pch = 19)
+
+#boxplot of count vs trap placement type
+boxplot(taban_df$count ~ taban_df$type)
+
+
 
 
